2cdb78e2c8d2e1576e6599398ca45734573d5ccc
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2020-05-14 11:24:13 +0200

    Add terms and conditions.

--- ftw/noticeboard/content/notice.py
+++ ftw/noticeboard/content/notice.py
@@ -1,9 +1,13 @@
+from ftw.noticeboard import _
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.dexterity.content import Container
 from plone.supermodel import model
+from z3c.form import validator
+from zope import schema
 from zope.interface import alsoProvides
 from zope.interface import implements
 from zope.interface import Interface
+from zope.interface import Invalid
 
 
 class INotice(Interface):
@@ -13,9 +17,31 @@ class INotice(Interface):
 class INoticeSchema(model.Schema):
     """A Folderish type for notices, which may hold images"""
 
+    accept_conditions = schema.Bool(
+        title=_(u'label_accept_conditions', default=u'Terms and Conditions'),
+        description=_(u'description_accept_conditions',
+                      default=u'Please accept the '
+                      '<a target="_blank" href="./terms_and_conditions">terms and conditions</a>'),
+        default=False)
+
 
 class Notice(Container):
     implements(INotice)
 
 
 alsoProvides(INoticeSchema, IFormFieldProvider)
+
+
+class AcceptedTermsAndConditions(validator.SimpleFieldValidator):
+    """ z3c.form validator class for international phone numbers """
+
+    def validate(self, value):
+        if not value:
+            raise Invalid(_(u'You need to accept the terms and conditions'))
+        return
+
+
+validator.WidgetValidatorDiscriminators(
+    AcceptedTermsAndConditions,
+    field=INoticeSchema['accept_conditions']
+)
--- ftw/noticeboard/tests/test_add_content.py
+++ ftw/noticeboard/tests/test_add_content.py
@@ -3,14 +3,19 @@ from ftw.builder import create
 from ftw.noticeboard.tests import FunctionalTestCase
 from ftw.testbrowser import browsing
 from ftw.testbrowser.pages import factoriesmenu
+from ftw.testbrowser.pages import statusmessages
 from ftw.testbrowser.pages import plone
+from plone.app.textfield.value import RichTextValue
 
 
 class TestContentTypes(FunctionalTestCase):
 
+    def setUp(self):
+        super(TestContentTypes, self).setUp()
+        self.grant('Manager')
+
     @browsing
     def test_add_noticeboard(self, browser):
-        self.grant('Manager')
         browser.login().visit()
         factoriesmenu.add('NoticeBoard')
         browser.fill({'Title': u'This is our NoticeBoard'})
@@ -19,25 +24,39 @@ class TestContentTypes(FunctionalTestCase):
 
     @browsing
     def test_add_noticecategory(self, browser):
-        self.grant('Manager')
-
         noticeboard = create(Builder('noticeboard').titled(u'Noticeboard'))
 
         browser.login().visit(noticeboard)
         factoriesmenu.add('NoticeCategory')
-        browser.fill({'Title': u'This is a Category'})
+        browser.fill({'Title': u'This is a Category', 'Terms and Conditions': 'Anything'})
         browser.find_button_by_label('Save').click()
         self.assertEquals(u'This is a Category', plone.first_heading())
 
     @browsing
     def test_add_notice(self, browser):
-        self.grant('Manager')
-
         noticeboard = create(Builder('noticeboard').titled(u'Noticeboard'))
-        category = create(Builder('noticecategory').titled(u'Category').within(noticeboard))
+        category = create(Builder('noticecategory')
+                          .having(conditions=RichTextValue('Something'))
+                          .titled(u'Category')
+                          .within(noticeboard))
+
+        browser.login().visit(category)
+        factoriesmenu.add('Notice')
+        browser.fill({'Title': u'This is a Notice', 'Terms and Conditions': True})
+        browser.find_button_by_label('Save').click()
+        self.assertEquals(u'This is a Notice', plone.first_heading())
+
+    @browsing
+    def test_terms_and_conditions_need_to_be_accepted(self, browser):
+        noticeboard = create(Builder('noticeboard').titled(u'Noticeboard'))
+        category = create(Builder('noticecategory')
+                          .having(conditions=RichTextValue('Something'))
+                          .titled(u'Category')
+                          .within(noticeboard))
 
         browser.login().visit(category)
         factoriesmenu.add('Notice')
         browser.fill({'Title': u'This is a Notice'})
         browser.find_button_by_label('Save').click()
-        self.assertEquals(u'This is a Notice', plone.first_heading())
+        self.assertEquals(u'Add Notice', plone.first_heading())
+        statusmessages.assert_message('There were some errors.')
