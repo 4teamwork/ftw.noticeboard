f21ddd535f9c73d29501a2316a30f296a59dad3b
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2020-05-18 11:03:46 +0200

    Add collective.deletepermission in order to allow a owner to delete its own notices.

--- ftw/noticeboard/tests/test_noticeboard_workflow.py
+++ ftw/noticeboard/tests/test_noticeboard_workflow.py
@@ -2,6 +2,7 @@ from ftw.builder import Builder
 from ftw.builder import create
 from ftw.noticeboard.tests import FunctionalTestCase
 from ftw.testbrowser import browsing
+from ftw.testbrowser.exceptions import InsufficientPrivileges
 from ftw.testbrowser.pages import factoriesmenu
 from ftw.testbrowser.pages import plone
 from plone.app.textfield.value import RichTextValue
@@ -85,3 +86,39 @@ class TestNoticeBoardWorkflow(FunctionalTestCase):
         self.assertEqual(
             ['NoticeImage', ],
             factoriesmenu.addable_types())
+
+    @browsing
+    def test_user_can_delete_its_notices_but_not_from_others(self, browser):
+        user = create(Builder('user'))
+
+        noticeboard = create(Builder('noticeboard'))
+        category = create(Builder('noticecategory').within(noticeboard))
+        othernotice = create(Builder('notice')
+                             .titled(u'This is a Notice')
+                             .having(accept_conditions=True,
+                                     text=RichTextValue('Something'),
+                                     price='100')
+                             .within(category))
+
+        with self.assertRaises(InsufficientPrivileges):
+            browser.login(user).visit(othernotice, view='delete_confirmation')
+            browser.find_button_by_label('Delete').click()
+
+        browser.visit(category)
+        factoriesmenu.add('Notice')
+        browser.fill(
+            {
+                'Title': u'This is a Notice',
+                'Price': '100',
+                'Terms and Conditions': True,
+                'E-Mail': u'hans@peter.example',
+                'Text': u'Anything',
+            }
+        )
+        browser.find_button_by_label('Save').click()
+        notice = browser.context
+
+        browser.visit(notice, view='delete_confirmation')
+        browser.find_button_by_label('Delete').click()
+
+        self.assertNotIn(notice, category.objectValues())
